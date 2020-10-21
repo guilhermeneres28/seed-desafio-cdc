82747ca4b782df452942a957e22baa66458e2fff
Guilherme Neres <guilhermeneresbsb@gmail.com>
2020-10-21 15:07:03 -0300

    Iniciando o projeto

---
+++ src/main/kotlin/br/com/deveficiente/desafiocasadocodigo/DeveficienteCasadocodigoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.deveficiente.desafiocasadocodigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeveficienteCasadocodigoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeveficienteCasadocodigoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/deveficiente/desafiocasadocodigo/DeveficienteCasadocodigoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.deveficiente.desafiocasadocodigo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeveficienteCasadocodigoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

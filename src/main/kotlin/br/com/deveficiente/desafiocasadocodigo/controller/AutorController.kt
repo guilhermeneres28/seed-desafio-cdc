a3e43e11f0cd6c14397f7b33f056def16421d8df
Guilherme Neres <guilhermeneresbsb@gmail.com>
2020-10-21 19:43:11 -0300

    Criando controller de autor

---
+++ src/main/kotlin/br/com/deveficiente/desafiocasadocodigo/controller/AutorController.kt
@@ -0,0 +1,20 @@
+package br.com.deveficiente.desafiocasadocodigo.controller
+
+import br.com.deveficiente.desafiocasadocodigo.dto.AutorDTO
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/autores")
+class AutorController {
+
+    private val log = LoggerFactory.getLogger(AutorController::class.java);
+
+    @PostMapping
+    fun salvar(@RequestBody autorDTO: AutorDTO) {
+        log.info("Cadastrando autor: $autorDTO")
+    }
+}
---
+++ src/main/kotlin/br/com/deveficiente/desafiocasadocodigo/dto/AutorDTO.kt
@@ -0,0 +1,7 @@
+package br.com.deveficiente.desafiocasadocodigo.dto
+
+data class AutorDTO(
+        val nome: String,
+        val email: String,
+        val descricao: String
+)
